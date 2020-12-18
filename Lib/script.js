81a9e8fa5ecb3dd6edc1e0cc69d36a847547cb9e
wisperMC4 <60354718+wisperMC4@users.noreply.github.com>
2020-12-18 13:08:07 +0000

    Add files via upload

---
+++ Lib/script.js
@@ -0,0 +1,91 @@
+$(document).ready(function(){
+
+    // set the checkout figure
+    if (localStorage.getItem('checkout') == null) 
+    {  
+        localStorage.setItem('checkout',0);
+    }
+    $("#checkout" ).html(localStorage.getItem('checkout'));
+
+     // check if user is logged in or logged out..
+     var loggedin=localStorage.getItem('loggedIn');
+    
+    //Add to cart Count
+    $(".shopbut").click(function(){
+        var total=localStorage.getItem('checkout');
+        total++;
+        localStorage.setItem('checkout',total);
+        $("#checkout" ).html(total );
+    });
+
+
+    //Login 
+    $('form[name="Login"]' ).submit(function( event ) 
+    {
+        var email=$('input[name="Email"]').val();
+        var password =$('input[name="Enter Password"]').val();
+        if (email=="johndoe@gmail.com" && password=="12345")  
+        {   
+            // successful login, user redirected to shop.html
+            localStorage.setItem('loggedIn',1);    
+            window.location.href = "Shop.html";  // redirect to shop page
+        }
+        else 
+        {
+            // login unsuccessful, error message appears
+            localStorage.setItem('loggedIn',0);
+            $("#loginER").addClass("d-block");
+        }
+        return false;
+    });  
+
+
+    if(loggedin==1) 
+    {
+        $("#loginIcon").addClass('iconChange');//adds a class to the login icon so when its logged in it changes colour
+
+        $("#accountdetails").removeClass( "d-none" ); //adds the dropdown menu when logged in 
+        $("#accountdetails").removeClass( "show" );
+    }
+    else
+    {
+        $("#loginIcon").removeClass('iconChange');//Brings the login icon to its default state
+
+        $("#accountdetails").addClass("d-none"); //Removes the login and user details 
+    }
+   
+   
+    $("#loginlogout").button().click(function(){
+        if (loggedin==1) {
+            // set the flag so that user is not logged in
+            localStorage.setItem('loggedIn',0);
+            window.location.href = "login.html";
+        }  else 
+            window.location.href = "login.html";
+
+    });  
+
+    //Data Validation
+    if (localStorage.getItem('userdetails') === null) {  
+        // if userdetails is null, that means it has not been loaded before. we not initialise userdetails object
+        var userDetails = {firstName:"John", lastName:"Doe", dob:"1990-12-01",Street:"Down the Lane", Town:"On yon road", County:"Co. Leitrim"};
+        // now we store the userdetails object as a localstorage object but localstore only stores text and userdetails is a javascript object
+        // we convert a javascript object ot a string using JSON.stringify - we are being expedient!
+        localStorage.setItem('userdetails',JSON.stringify(userDetails));
+    } else {
+        // if localstorage variable userdetails is already created - load it to javascript oject. JSON.parse turns it back into an javascript object
+        userDetails=JSON.parse(localStorage.getItem('userdetails'));
+    }
+
+    // we only run this code if an id of udetails is on the html page we are currently on - makes the code a little bit more efficient
+    // if the length > 0 it means we are on the right page - and we can populdate the form fields!!!
+    if ($('#udetails').length > 0) {
+        $('input[name="FirstName"]').val(userDetails.firstName);         
+        $('input[name="LastName"]').val(userDetails.lastName);
+        $('input[name="dob"]').val(userDetails.dob);
+        $('input[name="Street"]').val(userDetails.address1);
+        $('input[name="Town"]').val(userDetails.address2);
+        $('input[name="County"]').val(userDetails.address3);
+    }
+
+}); 
